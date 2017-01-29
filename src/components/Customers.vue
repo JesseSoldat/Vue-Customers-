72786ef1f090d6f523a978b89494503804fd7ee3
jesse <jsoldat@hotmail.com>
2017-01-29 12:01:40 +0700

    set up router and basic ui

---
+++ src/components/Customers.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="customers container">
+    <h1 class="page-header">Manage Customers</h1>
+    <input class="form-control" placeholder="Enter Last Name">
+    <br>
+    <table class="table tabel-striped">
+    	<thead>
+    		<tr>
+    			<th>First Name</th>
+    			<th>Last Name</th>
+    			<th>Email</th>
+    			<th></th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<tr>
+    			<td></td>
+    		</tr>
+    	</tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customers',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
